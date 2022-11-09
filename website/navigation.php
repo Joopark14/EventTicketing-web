0979ac4eeceaeff08c9f6d21e83ad9e33c597417
Michał Pastuszak <mpastuszak5@gmail.com>
2022-11-09 15:48:01 +0100

    added font source

--- website/navigation.php
+++ website/navigation.php
@@ -17,6 +17,9 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
 
+     <!-- css fonts source -->
+     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
     <!-- css source path -->
     <link rel="stylesheet" href="./styles/nav.css">
 </head>
