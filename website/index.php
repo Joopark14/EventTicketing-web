c733b73b3827e45890782ea33b6b2982b95c69e7
Joopark14 <107700964+Joopark14@users.noreply.github.com>
2022-11-13 17:43:54 +0100

    Search bar for events page
    Started developing the events page. The data can be displayed now. Which means the events can be displayed. Search bar is still in progress.

--- website/index.php
+++ website/index.php
@@ -1,4 +1,12 @@
 <?php
     include "navigation.php";
+?>
+<!-- THIS IS TEST CODE FOR IMPLEMENTING EVENTS PAGE -->
+    <form method="POST" action="events.php">
+        <a href="events.php"><button type="submit" value="football" name="event_cat">football</button></a>
+        <a href="events.php"><button type="submit" value="basketball" name="event_cat">basketball</button></a>
+    </form>
 
+</body>
+</html>
     
